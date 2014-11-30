ccb6348a1739b37369735dc7156cb0730fc77ac7
Walter <coolwaterever@gmail.com>
2014-11-30 13:23:33 +0800

    init

---
+++ Chat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Chat
+//
+//  Created by WalterInSH on 11/30/14.
+//  Copyright (c) 2014 WalterInSH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Chat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Chat
+//
+//  Created by WalterInSH on 11/30/14.
+//  Copyright (c) 2014 WalterInSH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
